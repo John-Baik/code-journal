bf899e45c9b248341b8e69afd1a17b0370a810b3
John Baik <johnwbaik@gmail.com>
2021-10-27 18:47:18 -0700

    Use a loop to create a DOM tree for each journal entry in the data model and append it to the page when the 'DOMContentLoaded' event is fired.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 /* exported data */
 var journalForm = document.querySelector('#journal');
 
+var entries = document.querySelector('ul');
 var upload = document.getElementById('upload');
 var photo = document.getElementById('photo');
 var form = document.forms[0];
@@ -23,4 +24,37 @@ form.addEventListener('submit', function (event) {
   data.entries.unshift(object);
   journalForm.reset();
   upload.setAttribute('src', 'images/placeholder-image-square.jpg');
+});
+
+function renderEntries(entry) {
+  var li = document.createElement('li');
+  li.setAttribute('class', 'row');
+  var colHalf = document.createElement('div');
+  colHalf.setAttribute('class', 'column-half');
+  var image = document.createElement('img');
+  image.setAttribute('src', entry.photoInput);
+  var colHalf2 = document.createElement('div');
+  colHalf2.setAttribute('class', 'column-half');
+  var bodyTitle = document.createElement('div');
+  bodyTitle.setAttribute('class', 'body-title');
+  var h2 = document.createElement('h2');
+  h2.setAttribute('class', 'entry-title');
+  var info = document.createElement('div');
+  info.setAttribute('class', 'info');
+  var infoText = document.createElement('p');
+  infoText.textConent = entry.notesInput;
+  li.appendChild(colHalf);
+  colHalf.appendChild(image);
+  li.appendChild(colHalf2);
+  colHalf2.appendChild(bodyTitle);
+  bodyTitle.appendChild(h2);
+  colHalf2.appendChild(info);
+  info.appendChild(infoText);
+  return li;
+}
+
+window.addEventListener('DOMContentLoaded', function (event) {
+  for (var i = 0; i < data.entries.length; i++) {
+    entries.appendChild(renderEntries(data.entries[i]));
+  }
 });
